c8b506a2c9396727b0185f99a7995a43efcc2db3
Frank Schneider <shoul@gmx.de>
2024-09-09 10:30:12 +0200

    Adding phys. layers and masks and implement player jumping.

--- player.gd
+++ player.gd
@@ -4,6 +4,8 @@ extends CharacterBody3D
 @export var speed: int = 14
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration: int = 75
+# Vertical impuleapplied to the character upon jumping in meter per second.
+@export var jump_impulse: int = 20
 
 var target_velocity: Vector3 = Vector3.ZERO
 
@@ -31,6 +33,10 @@ func _physics_process(delta):
 	# Vertical Velocity
 	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+
+	# Jumping.
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		target_velocity.y = jump_impulse
 
 	# Moving the Character
 	velocity = target_velocity
