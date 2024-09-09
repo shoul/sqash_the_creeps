db64d51db0fd057155162671860d44552e72fa81
Frank Schneider <shoul@gmx.de>
2024-09-09 13:30:53 +0200

    Add squash mob functionality.

--- mob.gd
+++ mob.gd
@@ -1,9 +1,16 @@
 extends CharacterBody3D
+# Emitted when the player jumped on the mob.
+signal squashed
 
 # Minimum speed of the mob in m / s
 @export var min_speed: int = 10
 # Maximum speed of the mob in m / s
 @export var max_speed: int = 18
+
+
+func squash():
+	squashed.emit()
+	queue_free()
 
 
 func _physics_process(_delta):
@@ -18,7 +25,7 @@ func initialize(start_position, player_position):
 	# Rotate this mob randomly within range of -45 and +45 degrees,
 	# so that is doesn't move directly towards the player every time.
 	rotate_y(randf_range(-PI / 4, PI / 4))
-	
+
 	# We calculate a random speed (integer)
 	var random_speed: int = randi_range(min_speed, max_speed)
 	# We calculate a forward velocity that represents the speed.
